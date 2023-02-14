1762e01be50ca3a461b9523570731cdf081d15f8
Lucas Wermann <lw1707@hotmail.com>
2023-02-13 22:06:02 -0300

    Deploy attempt

--- next.config.js
+++ next.config.js
@@ -1,10 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+	reactStrictMode: true,
+	distDir: '.next',
 
-  images: {
-    domains: ['files.stripe.com'],
-  },
+	images: {
+		domains: ['files.stripe.com'],
+	},
 }
 
 module.exports = nextConfig
